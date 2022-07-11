b860bac25c5ffe7b399c7389a686d74be55c371e
LeeTomahawk <bartek20122013@gmail.com>
2022-07-11 14:16:54 +0200

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,24 +8,33 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [AppComponent, NavMenuComponent, HomeComponent, EmployeeListComponent, TasksListComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    EmployeeListComponent,
+    TasksListComponent,
+    LoginComponent,
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     MatPaginatorModule,
+    MatSidenavModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'employee-list', component: EmployeeListComponent },
       { path: 'tasks-list', component: TasksListComponent },
       { path: 'login', component: LoginComponent },
-      { path: 'profile', component: ProfileComponent}
+      { path: 'profile', component: ProfileComponent },
     ]),
     BrowserAnimationsModule,
   ],
