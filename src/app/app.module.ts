99738436d2ddd1c45182013d80d21277b7184783
LeeTomahawk <bartek20122013@gmail.com>
2022-07-19 12:32:18 +0200

    Login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,6 +30,9 @@ import {MatButtonModule} from '@angular/material/button';
     LoginComponent,
   ],
   imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
     MatButtonModule,
     MatIconModule,
     MatListModule,
