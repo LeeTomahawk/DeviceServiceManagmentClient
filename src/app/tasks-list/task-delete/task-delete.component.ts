d8ef1be67ff6f0519104dc78e4c1f4ff0f9b7e2a
LeeTomahawk <bartek20122013@gmail.com>
2022-07-20 10:54:39 +0200

    task-delete

--- src/app/tasks-list/task-delete/task-delete.component.ts
+++ src/app/tasks-list/task-delete/task-delete.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-task-delete',
   templateUrl: './task-delete.component.html',
-  styleUrls: ['./task-delete.component.css']
+  styleUrls: ['./task-delete.component.css'],
 })
 export class TaskDeleteComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { taskId: string }) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
