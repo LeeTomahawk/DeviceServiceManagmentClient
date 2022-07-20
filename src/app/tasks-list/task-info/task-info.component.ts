e3bbb60d56341fd8cbe5664378345e8c13706e94
LeeTomahawk <bartek20122013@gmail.com>
2022-07-20 09:15:03 +0200

    Task table, task info

--- src/app/tasks-list/task-info/task-info.component.ts
+++ src/app/tasks-list/task-info/task-info.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TaskInfo } from '../taskInterfaces';
 const ELEMENT_DATA: TaskInfo = {
-  name: 'asd',
+  name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   description:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pulvinar est leo, et facilisis risus sollicitudin non. Curabitur pellentesque dignissim nibh ut sodales. Donec in nulla luctus augue euismod maximus ac vitae lorem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis at sodales sem. Nulla pharetra sagittis justo, vitae convallis metus ornare vel. Phasellus rhoncus vestibulum massa sed suscipit. Curabitur placerat malesuada est et euismod. Aliquam dictum commodo neque nec ultricies. Duis urna mauris, interdum non sodales non, accumsan in ex. Donec nec neque scelerisque neque facilisis fringilla. Etiam laoreet ipsum venenatis vulputate maximus. Cras egestas neque id mattis ultricies. Sed non nulla nec nulla posuere molestie.',
   startDate: '12-12-1233',
