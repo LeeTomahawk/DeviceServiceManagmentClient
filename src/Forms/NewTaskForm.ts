ed809f03bb4f9d4237c49c6d613fc9fa15ee2a53
LeeTomahawk <bartek20122013@gmail.com>
2022-08-07 20:14:22 +0200

    Add NewTaskForm

--- src/Forms/NewTaskForm.ts
+++ src/Forms/NewTaskForm.ts
@@ -6,7 +6,7 @@ export class NewTaskForm extends FormGroup {
       name: new FormControl(null, [Validators.required]),
       description: new FormControl(null, [Validators.required]),
       clientId: new FormControl(null, [Validators.required]),
-      startDate: new FormControl(new Date(), [Validators.required]),
+      startDate: new FormControl(null, [Validators.required]),
       taskStatus: new FormControl('RECEIVED', [Validators.required]),
     });
   }
