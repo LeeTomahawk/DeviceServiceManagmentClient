89b20b8bb95604140167ff0952871e235a3133a7
LeeTomahawk <bartek20122013@gmail.com>
2022-09-04 10:53:30 +0200

    Paggination

--- src/app/manager-list/manager-list.component.ts
+++ src/app/manager-list/manager-list.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ManagerService } from 'src/Services/manager.service';
+import { ClientUpdateComponent } from '../client-list/client-update/client-update.component';
 import { Manager } from './managerInterface';
 
 @Component({
@@ -12,15 +15,23 @@ import { Manager } from './managerInterface';
 export class ManagerListComponent implements OnInit {
   displayedColumns: string[] = [
     'position',
-    'name',
-    'phoneNumber',
-    // 'tasks',
-    // 'info',
+    'LastName',
+    'PhoneNumber',
     'actions',
   ];
   dataSource = new MatTableDataSource<Manager>();
   managerList: any;
   isLoading: boolean = true;
+  totalSize: number = 0;
+  SearchPharse: string = '';
+  PageNumber: number = 1;
+  PageSize: number = 25;
+  SortBy: string = '';
+  SortDirection: string = '';
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild('empTbSort') empTbSort = new MatSort();
+
   constructor(
     private infoDialog: MatDialog,
     private updateDialog: MatDialog,
@@ -29,15 +40,53 @@ export class ManagerListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.getManagerList();
+    const request: any = {};
+    request.SearchPharse = this.SearchPharse;
+    request.PageNumber = this.PageNumber;
+    request.PageSize = this.PageSize;
+    request.SortBy = this.SortBy;
+    request.SortDirection = this.SortDirection;
+    this.getManagerList(request);
   }
 
-  getManagerList() {
-    this.managerApiCaller.getManagerList().subscribe((data) => {
-      this.managerList = data;
+  getManagerList(params: any) {
+    this.managerApiCaller.getManagerList(params).subscribe((data) => {
+      this.managerList = data.result;
+      this.totalSize = data.totalResult;
       this.dataSource = new MatTableDataSource<Manager>(this.managerList);
+      this.dataSource.sort = this.empTbSort;
       this.isLoading = false;
     });
+  }
+
+  nextPage(event: PageEvent) {
+    this.PageNumber = event.pageIndex + 1;
+    this.PageSize = event.pageSize;
+
+    const request: any = {};
+
+    request.SearchPharse = this.SearchPharse;
+    request.PageNumber = this.PageNumber;
+    request.PageSize = this.PageSize;
+    request.SortBy = this.SortBy;
+    request.SortDirection = this.SortDirection;
+
+    this.getManagerList(request);
+  }
+
+  onSortChanged(event: Sort) {
+    this.SortBy = event.active;
+    this.SortDirection = event.direction;
+
+    const request: any = {};
+
+    request.SearchPharse = this.SearchPharse;
+    request.PageNumber = this.PageNumber;
+    request.PageSize = this.PageSize;
+    request.SortBy = this.SortBy;
+    request.SortDirection = this.SortDirection;
+
+    this.getManagerList(request);
   }
 
   openInfoDialog(manager: Manager) {
@@ -47,12 +96,21 @@ export class ManagerListComponent implements OnInit {
   }
 
   openUpdateDialog(manager: Manager) {
-    // const dialogRef = this.updateDialog.open(, {
-    //   data: { manager },
-    // });
-    // dialogRef.afterClosed().subscribe((x) => {
-    //   window.location.reload();
-    // });
+    const dialogRef = this.updateDialog.open(ClientUpdateComponent, {
+      data: { manager },
+    });
+
+    dialogRef.afterClosed().subscribe((x) => {
+      const request: any = {};
+
+      request.SearchPharse = this.SearchPharse;
+      request.PageNumber = this.PageNumber;
+      request.PageSize = this.PageSize;
+      request.SortBy = this.SortBy;
+      request.SortDirection = this.SortDirection;
+
+      this.getManagerList(request);
+    });
   }
 
   openTasksDialog(manager: Manager) {
