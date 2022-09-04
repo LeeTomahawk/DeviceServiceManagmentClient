8b0425465390b4b87c8d59c66c53a444c822bfbc
LeeTomahawk <bartek20122013@gmail.com>
2022-09-04 10:53:45 +0200

    Paggination

--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -2,8 +2,8 @@ import { DatePipe } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { TaskListCalendarForm } from 'src/Forms/TaskListCalendarForm';
 import { TaskService } from 'src/Services/task.service';
@@ -23,14 +23,20 @@ export class TasksListComponent implements OnInit {
   form = new TaskListCalendarForm();
   displayedColumns: string[] = [
     'position',
-    'name',
-    'status',
-    'data',
+    'Name',
+    'TaskStatus',
+    'startDate',
     'actions',
   ];
   taskList: any;
   dataSource!: MatTableDataSource<Task>;
   isLoading: boolean = true;
+  totalSize: number = 0;
+  SearchPharse: string = '';
+  PageNumber: number = 1;
+  PageSize: number = 25;
+  SortBy: string = '';
+  SortDirection: string = '';
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild('empTbSort') empTbSort = new MatSort();
@@ -50,21 +56,67 @@ export class TasksListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getTaskList();
+    const request: any = {};
+    request.SearchPharse = this.SearchPharse;
+    request.PageNumber = this.PageNumber;
+    request.PageSize = this.PageSize;
+    request.SortBy = this.SortBy;
+    request.SortDirection = this.SortDirection;
+
+    this.getTaskList(request);
   }
 
-  async getTaskList() {
+  async getTaskList(params: any) {
     await this.taskApiCaller
       .getTaskList(
         this.form.get('startDate')?.value,
-        this.form.get('endDate')?.value
+        this.form.get('endDate')?.value,
+        params
       )
       .then((data) => {
-        this.taskList = data;
+        this.taskList = data.result;
+        this.totalSize = data.totalResult;
         this.dataSource = new MatTableDataSource<Task>(this.taskList);
-        if (this.taskList.length > 0) this.isLoading = false;
+        this.dataSource.sort = this.empTbSort;
+        if (this.totalSize > 0) this.isLoading = false;
         else this.isLoading = true;
       });
+  }
+
+  applyFilter(event: Event) {
+    this.SearchPharse = (event.target as HTMLInputElement).value;
+
+    this.ngOnInit();
+  }
+
+  async nextPage(event: PageEvent) {
+    this.PageNumber = event.pageIndex + 1;
+    this.PageSize = event.pageSize;
+
+    const request: any = {};
+
+    request.SearchPharse = this.SearchPharse;
+    request.PageNumber = this.PageNumber;
+    request.PageSize = this.PageSize;
+    request.SortBy = this.SortBy;
+    request.SortDirection = this.SortDirection;
+
+    await this.getTaskList(request);
+  }
+
+  async onSortChanged(event: Sort) {
+    this.SortBy = event.active;
+    this.SortDirection = event.direction;
+
+    const request: any = {};
+
+    request.SearchPharse = this.SearchPharse;
+    request.PageNumber = this.PageNumber;
+    request.PageSize = this.PageSize;
+    request.SortBy = this.SortBy;
+    request.SortDirection = this.SortDirection;
+
+    await this.getTaskList(request);
   }
 
   openInfoDialog(task: Tasks) {
@@ -79,7 +131,15 @@ export class TasksListComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe((x) => {
-      this.getTaskList();
+      const request: any = {};
+
+      request.SearchPharse = this.SearchPharse;
+      request.PageNumber = this.PageNumber;
+      request.PageSize = this.PageSize;
+      request.SortBy = this.SortBy;
+      request.SortDirection = this.SortDirection;
+
+      this.getTaskList(request);
     });
   }
 
@@ -89,7 +149,25 @@ export class TasksListComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe((x) => {
-      this.getTaskList();
+      const request: any = {};
+
+      request.SearchPharse = this.SearchPharse;
+      request.PageNumber = this.PageNumber;
+      request.PageSize = this.PageSize;
+      request.SortBy = this.SortBy;
+      request.SortDirection = this.SortDirection;
+
+      this.getTaskList(request);
     });
+  }
+
+  taskStatusMap(value: string) {
+    if (value == 'RECEIVED') return 'PRZYJĘTO';
+    else if (value == 'WAITING_FOR_CLIENT') return 'OCZEKIWANIE NA ODBIÓR';
+    else if (value == 'IN_REPAIR') return 'W NAPRAWIE';
+    else if (value == 'REPAIRED') return 'NAPRAWIONE';
+    else if (value == 'NOT_REPAIRED') return 'NIE NAPRAWIONE';
+    else if (value == 'COLLECTED') return 'ODEBRANE';
+    return value;
   }
 }
